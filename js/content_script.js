e5c849caf6bc5b7a94cd19f0a6add92df98f84a6
Brian Culver <39041285+gandalfthegrape@users.noreply.github.com>
2022-12-11 14:48:19 -0600

    add storage saving

--- js/content_script.js
+++ js/content_script.js
@@ -2,21 +2,29 @@ console.log("Running content_script.js");
 // chrome.tabs.query({ active: true }, tab => {
 //     chrome.tabs.executeScript(tab.id, { file: "applyDefaultHighlights.js" });
 // })
+var currentElement;
 
-function showMenu(element) {
+function showMenu() {
+    var text = currentElement.innerText;
+
     var menuDiv = document.createElement("div");
     menuDiv.setAttribute("id", "contextMenu");
-    //menuDiv.setAttribute("class", "hidden");
-    menuDiv.innerHTML =
-        `<ul>
-            <li>menu item</li>
-            <li>menu item</li>
-            <li>menu item</li>
-            <li>menu item</li>
-            <li>menu item</li>
-            <li>menu item</li>
-        </ul>`;
-    element.appendChild(menuDiv);
+    menuDiv.setAttribute("class", "noHideOnClick");
+    menuDiv.innerHTML = `${text}<br>`;
+
+    var textInput = document.createElement("input");
+    textInput.setAttribute("type", "userDefinition");
+    textInput.setAttribute("class", "noHideOnClick");
+    menuDiv.appendChild(textInput);
+
+    var okButton = document.createElement("input");
+    okButton.setAttribute("type", "button");
+    okButton.setAttribute("class", "noHideOnClick");
+    okButton.setAttribute("value", "OK");
+    okButton.onclick = function () { saveNote(text, textInput.value) };
+    menuDiv.appendChild(okButton);
+
+    currentElement.appendChild(menuDiv);
 }
 
 function hideMenu() {
@@ -30,14 +38,46 @@ function hideMenu() {
 document.addEventListener("contextmenu", function(event) {
   // Only do something when the element that was actually right-clicked
   // on has the right class to trigger the menu
-  if(event.target.classList.contains("defaultHighlight")){
-    event.preventDefault();
-    showMenu(event.target);
+    if (event.target.classList.contains("defaultHighlight")) {
+        hideMenu();
+        event.preventDefault();
+        currentElement = event.target;
+        showMenu();
   }
 });
 
 // Close the menu when you left click anywhere
-document.addEventListener("click", function(event){
+document.addEventListener("click", function (event) {
     //document.getElementById("contextMenu").classList.add("hidden");
-    hideMenu();
+    if (!(event.target.classList.contains("noHideOnClick"))) {
+        hideMenu();
+    };
 });
+
+///////////////////////////
+
+//handling chrome.storage
+
+var highlights;
+var language = "English";
+
+function saveNote(word, note) {
+    console.log("getStorage");
+    chrome.storage.local.get("highlights", results => {
+        if (results.highlights) {
+            highlights = results.highlights;
+        } else {
+            highlights = {};
+        }
+        if (!highlights[language]) {
+            highlights[language] = {};
+        };
+        console.log(`Saving ${language} ${word} with note ${note}`)
+        highlights[language][word] = [
+            note
+        ];
+        chrome.storage.local.set({ highlights }, () => { });
+        console.log("storage saved");
+    })
+};
+

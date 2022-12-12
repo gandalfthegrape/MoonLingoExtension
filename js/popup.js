a5456264e41ee1d5a21b2bcdedb49169223cef02
Brian Culver <39041285+gandalfthegrape@users.noreply.github.com>
2022-12-11 18:49:08 -0600

    add send message on button click

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,13 @@
-console.log("Running content_script.js");
+console.log("Running popup.js");
 
 document.getElementById("applyDefaultHighlights").addEventListener('click',
     function () {
-        chrome.tabs.query({ active: true }, tab => {
-            chrome.tabs.executeScript(tab.id, { file: "js/applyDefaultHighlights.js" });
+        console.log("sending message");
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, { "message": "applyDefaultHighlights" });
+            console.log("message sent");
+            //chrome.tabs.executeScript(tab.id, { file: "js/applyDefaultHighlights.js" });
         });
     }
 );
